c47988e4e3ddb6237e5d617b51cdd65c0ab18d81
aaron <aaronhowser1@gmail.com>
2026-07-27 10:51:38 -0500

    assets

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/datagen/language/ModLanguageProvider.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/datagen/language/ModLanguageProvider.kt
@@ -3,6 +3,7 @@ package dev.aaronhowser.mods.critter_carts.datagen.language
 import dev.aaronhowser.mods.critter_carts.CritterCarts
 import dev.aaronhowser.mods.critter_carts.registry.ModBlocks
 import dev.aaronhowser.mods.critter_carts.registry.ModCreativeModeTabs
+import dev.aaronhowser.mods.critter_carts.registry.ModEntityTypes
 import net.minecraft.data.PackOutput
 import net.neoforged.neoforge.common.data.LanguageProvider
 
@@ -12,6 +13,7 @@ class ModLanguageProvider(
 
 	override fun addTranslations() {
 		addBlock(ModBlocks.SCOOCHSTEM, "Scoochstem")
+		addEntityType(ModEntityTypes.SCOOCHWORM, "Scoochworm")
 		add(ModCreativeModeTabs.CREATIVE_TAB_TRANSLATION_KEY, "Critter Carts")
 	}
 }
