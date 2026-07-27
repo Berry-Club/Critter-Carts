8fb7a70c64ec726e5c4ef94f05b65225603bc11b
aaron <aaronhowser1@gmail.com>
2026-07-27 11:19:57 -0500

    spawn egg look away

---
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/item/ScoochwormSpawnEggItem.kt
@@ -0,0 +1,76 @@
+package dev.aaronhowser.mods.critter_carts.item
+
+import dev.aaronhowser.mods.critter_carts.entity.ScoochwormEntity
+import net.minecraft.core.Direction
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.entity.EntityType
+import net.minecraft.world.entity.MobSpawnType
+import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.world.level.Spawner
+import net.minecraft.world.level.gameevent.GameEvent
+import net.neoforged.neoforge.common.DeferredSpawnEggItem
+import net.neoforged.neoforge.registries.DeferredHolder
+
+class ScoochwormSpawnEggItem(
+	entityType: DeferredHolder<EntityType<*>, EntityType<ScoochwormEntity>>,
+	backgroundColor: Int,
+	highlightColor: Int,
+	properties: Properties
+) : DeferredSpawnEggItem(
+	entityType,
+	backgroundColor, highlightColor,
+	properties
+) {
+
+	override fun useOn(context: UseOnContext): InteractionResult {
+		val level = context.level
+		if (level !is ServerLevel) return InteractionResult.SUCCESS
+
+		val itemStack = context.itemInHand
+		val clickedPosition = context.clickedPos
+		val clickedFace = context.clickedFace
+		val clickedState = level.getBlockState(clickedPosition)
+		val spawner = level.getBlockEntity(clickedPosition)
+
+		if (spawner is Spawner) {
+			spawner.setEntityId(getType(itemStack), level.random)
+			level.sendBlockUpdated(clickedPosition, clickedState, clickedState, 3)
+			level.gameEvent(context.player, GameEvent.BLOCK_CHANGE, clickedPosition)
+			itemStack.shrink(1)
+			return InteractionResult.CONSUME
+		}
+
+		val spawnPosition = if (clickedState.getCollisionShape(level, clickedPosition).isEmpty) {
+			clickedPosition
+		} else {
+			clickedPosition.relative(clickedFace)
+		}
+
+		val spawnedEntity = getType(itemStack).spawn(
+			level,
+			itemStack,
+			context.player,
+			spawnPosition,
+			MobSpawnType.SPAWN_EGG,
+			true,
+			clickedPosition != spawnPosition && clickedFace == Direction.UP
+		)
+
+		if (spawnedEntity != null) {
+			val player = context.player
+			if (spawnedEntity is ScoochwormEntity && player != null) {
+				val rotation = player.direction.toYRot()
+				spawnedEntity.yRot = rotation
+				spawnedEntity.yRotO = rotation
+				spawnedEntity.yBodyRot = rotation
+				spawnedEntity.yHeadRot = rotation
+			}
+
+			itemStack.shrink(1)
+			level.gameEvent(player, GameEvent.ENTITY_PLACE, clickedPosition)
+		}
+
+		return InteractionResult.CONSUME
+	}
+}
--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/registry/ModItems.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/registry/ModItems.kt
@@ -2,7 +2,8 @@ package dev.aaronhowser.mods.critter_carts.registry
 
 import dev.aaronhowser.mods.aaron.registry.AaronItemRegistry
 import dev.aaronhowser.mods.critter_carts.CritterCarts
-import net.minecraft.world.item.SpawnEggItem
+import dev.aaronhowser.mods.critter_carts.item.ScoochwormSpawnEggItem
+import net.minecraft.world.item.Item
 import net.neoforged.neoforge.registries.DeferredItem
 import net.neoforged.neoforge.registries.DeferredRegister
 
@@ -11,6 +12,15 @@ object ModItems : AaronItemRegistry() {
 	val ITEM_REGISTRY: DeferredRegister.Items = DeferredRegister.createItems(CritterCarts.MOD_ID)
 	override fun getItemRegistry(): DeferredRegister.Items = ITEM_REGISTRY
 
-	val SCOOCHWORM_SPAWN_EGG: DeferredItem<SpawnEggItem> =
-		registerSpawnEgg("scoochworm_spawn_egg", ModEntityTypes.SCOOCHWORM::get, 0x95E4ED, 0x6DCF72)
+	val SCOOCHWORM_SPAWN_EGG: DeferredItem<ScoochwormSpawnEggItem> =
+		register(
+			"scoochworm_spawn_egg",
+			builder = { properties: Item.Properties ->
+				ScoochwormSpawnEggItem(
+					ModEntityTypes.SCOOCHWORM,
+					0x95E4ED, 0x6DCF72,
+					properties
+				)
+			}
+		)
 }
