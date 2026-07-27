35663b02f8cc62a5e718272750bd82d01cbc1fb8
aaron <aaronhowser1@gmail.com>
2026-07-27 12:05:22 -0500

    rotation because it's not a LivingEntity

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/client/renderer/ScoochwormPartRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/client/renderer/ScoochwormPartRenderer.kt
@@ -1,10 +1,13 @@
 package dev.aaronhowser.mods.critter_carts.client.renderer
 
+import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.math.Axis
 import dev.aaronhowser.mods.critter_carts.client.model.ScoochwormPartModel
 import dev.aaronhowser.mods.critter_carts.entity.ScoochwormEntity
 import dev.aaronhowser.mods.critter_carts.entity.ScoochwormPartEntity
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.resources.ResourceLocation
+import net.minecraft.util.Mth
 import software.bernie.geckolib.renderer.GeoEntityRenderer
 
 class ScoochwormPartRenderer(
@@ -17,4 +20,21 @@ class ScoochwormPartRenderer(
 
 	override fun getTextureLocation(animatable: ScoochwormPartEntity): ResourceLocation =
 		ScoochwormRenderer.TEXTURE
+
+	override fun applyRotations(
+		animatable: ScoochwormPartEntity,
+		poseStack: PoseStack,
+		ageInTicks: Float,
+		rotationYaw: Float,
+		partialTick: Float,
+		nativeScale: Float
+	) {
+		val interpolatedYaw = Mth.rotLerp(
+			partialTick,
+			animatable.yRotO,
+			animatable.yRot
+		)
+
+		poseStack.mulPose(Axis.YP.rotationDegrees(180f - interpolatedYaw))
+	}
 }
