df37f4f082a2cf65f52cf1b53093f06c7d9b8662
aaron <aaronhowser1@gmail.com>
2026-07-27 16:10:04 -0500

    add a dye color and move to using a codec

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/entity/ScoochwormSegment.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/entity/ScoochwormSegment.kt
@@ -1,38 +1,52 @@
 package dev.aaronhowser.mods.critter_carts.entity
 
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.NbtOps
+import net.minecraft.world.item.DyeColor
+import java.util.Optional
 
 data class ScoochwormSegment(
-	var attachment: ScoochwormPartAttachment = ScoochwormPartAttachment.NONE
+	var attachment: ScoochwormPartAttachment = ScoochwormPartAttachment.NONE,
+	var dyeColor: DyeColor? = null
 ) {
 
 	fun save(): CompoundTag {
-		val tag = CompoundTag()
-		val encodedAttachment = ScoochwormPartAttachment.CODEC
-			.encodeStart(NbtOps.INSTANCE, attachment)
+		val maybeTag = CODEC
+			.encodeStart(NbtOps.INSTANCE, this)
 			.result()
 
-		if (encodedAttachment.isPresent) {
-			tag.put(ATTACHMENT_TAG, encodedAttachment.get())
-		}
-
-		return tag
+		return maybeTag
+			.map { it as CompoundTag }
+			.orElseGet(::CompoundTag)
 	}
 
 	companion object {
 		private const val ATTACHMENT_TAG = "Attachment"
+		private const val DYE_COLOR_TAG = "DyeColor"
+
+		val CODEC: Codec<ScoochwormSegment> = RecordCodecBuilder.create { instance ->
+			instance.group(
+				ScoochwormPartAttachment.CODEC
+					.optionalFieldOf(ATTACHMENT_TAG, ScoochwormPartAttachment.NONE)
+					.forGetter(ScoochwormSegment::attachment),
+				DyeColor.CODEC
+					.optionalFieldOf(DYE_COLOR_TAG)
+					.forGetter { Optional.ofNullable(it.dyeColor) }
+			).apply(instance) { attachment, dyeColor ->
+				ScoochwormSegment(
+					attachment,
+					dyeColor.orElse(null)
+				)
+			}
+		}
 
 		fun load(tag: CompoundTag): ScoochwormSegment {
-			val attachmentTag = tag.get(ATTACHMENT_TAG)
-				?: return ScoochwormSegment()
-
-			val attachment = ScoochwormPartAttachment.CODEC
-				.parse(NbtOps.INSTANCE, attachmentTag)
+			return CODEC
+				.parse(NbtOps.INSTANCE, tag)
 				.result()
-				.orElse(ScoochwormPartAttachment.NONE)
-
-			return ScoochwormSegment(attachment)
+				.orElseGet(::ScoochwormSegment)
 		}
 	}
 }
