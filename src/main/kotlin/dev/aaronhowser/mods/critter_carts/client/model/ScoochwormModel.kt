1c12bf837e9e2f4b234e92a61ea65d1c4c818d6e
aaron <aaronhowser1@gmail.com>
2026-08-21 18:40:35 -0500

    fix the face sliding

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/client/model/ScoochwormModel.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/client/model/ScoochwormModel.kt
@@ -1,10 +1,13 @@
 package dev.aaronhowser.mods.critter_carts.client.model
 
+import com.mojang.math.Axis
 import dev.aaronhowser.mods.critter_carts.CritterCarts
 import dev.aaronhowser.mods.critter_carts.entity.ScoochwormEntity
+import net.minecraft.core.Direction
 import net.minecraft.util.Mth
+import net.minecraft.world.phys.Vec3
+import org.joml.Vector3f
 import software.bernie.geckolib.animation.AnimationState
-import software.bernie.geckolib.cache.`object`.GeoBone
 import software.bernie.geckolib.constant.DataTickets
 import software.bernie.geckolib.model.DefaultedEntityGeoModel
 
@@ -20,8 +23,66 @@ class ScoochwormModel : DefaultedEntityGeoModel<ScoochwormEntity>(
 		val face = animationProcessor.getBone("face")
 		val entityData = animationState.getData(DataTickets.ENTITY_MODEL_DATA) ?: return
 
-		face.posY = Mth.map(entityData.headPitch, -90f, 90f, -3f, 3f)
-		face.posX = Mth.map(entityData.netHeadYaw, -90f, 90f, -2f, 2f)
+		val partialTick = animationState.partialTick
+
+		val bodyYaw = Mth.rotLerp(partialTick, animatable.yRotO, animatable.yRot)
+		val headYaw = Mth.rotLerp(partialTick, animatable.yHeadRotO, animatable.yHeadRot)
+
+		val worldLookDirection = Vec3.directionFromRotation(entityData.headPitch, headYaw)
+		val localLookDirection = getLocalLookDirection(
+			worldLookDirection,
+			animatable.supportDirection,
+			bodyYaw
+		)
+		val isAttachedToWall = animatable.supportDirection.axis != Direction.Axis.Y
+		val horizontalLookDirection = if (isAttachedToWall) {
+			localLookDirection.x
+		} else {
+			-localLookDirection.x
+		}
+		val verticalLookDirection = if (isAttachedToWall) {
+			localLookDirection.y
+		} else {
+			-localLookDirection.y
+		}
+
+		face.posY = Mth.map(
+			verticalLookDirection,
+			-1f,
+			1f,
+			-3f,
+			3f
+		)
+
+		face.posX = Mth.map(
+			horizontalLookDirection,
+			-1f,
+			1f,
+			-3f,
+			3f
+		)
+	}
+
+	private fun getLocalLookDirection(
+		worldLookDirection: Vec3,
+		supportDirection: Direction,
+		bodyYaw: Float
+	): Vector3f {
+		val lookDirection = worldLookDirection.toVector3f()
+
+		val modelRotation = when (supportDirection) {
+			Direction.DOWN -> Axis.YP.rotationDegrees(0f)
+			Direction.UP -> Axis.ZP.rotationDegrees(180f)
+			Direction.NORTH -> Axis.XP.rotationDegrees(90f)
+			Direction.SOUTH -> Axis.XP.rotationDegrees(-90f)
+			Direction.WEST -> Axis.ZP.rotationDegrees(-90f)
+			Direction.EAST -> Axis.ZP.rotationDegrees(90f)
+		}
+
+		val surfaceYaw = if (supportDirection == Direction.UP) -bodyYaw else bodyYaw
+		modelRotation.mul(Axis.YP.rotationDegrees(180f - surfaceYaw))
+
+		return modelRotation.conjugate().transform(lookDirection)
 	}
 
 }
