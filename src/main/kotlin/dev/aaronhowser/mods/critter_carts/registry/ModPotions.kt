303c11bbbe7d91ac80519917c42ed134c90cffa1
aaron <aaronhowser1@gmail.com>
2026-08-26 12:59:59 -0500

    Refactor dyed potion registration

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/registry/ModPotions.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/registry/ModPotions.kt
@@ -1,13 +1,12 @@
 package dev.aaronhowser.mods.critter_carts.registry
 
 import dev.aaronhowser.mods.critter_carts.CritterCarts
-import dev.aaronhowser.mods.critter_carts.entity.data.WormColor
-import net.minecraft.core.Holder
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.item.alchemy.Potion
 import net.minecraft.world.item.alchemy.Potions
 import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent
+import net.neoforged.neoforge.registries.DeferredHolder
 import net.neoforged.neoforge.registries.DeferredRegister
 import java.util.function.Supplier
 
@@ -18,42 +17,36 @@ object ModPotions {
 	val POTION_REGISTRY: DeferredRegister<Potion> =
 		DeferredRegister.create(Registries.POTION, CritterCarts.MOD_ID)
 
-	val DYED_GREEN: Holder<Potion> = registerDyed(WormColor.GREEN)
-	val DYED_BLUE: Holder<Potion> = registerDyed(WormColor.BLUE)
-	val DYED_RED: Holder<Potion> = registerDyed(WormColor.RED)
-	val DYED_YELLOW: Holder<Potion> = registerDyed(WormColor.YELLOW)
-	val DYED_MAGENTA: Holder<Potion> = registerDyed(WormColor.MAGENTA)
-	val DYED_CYAN: Holder<Potion> = registerDyed(WormColor.CYAN)
+	val DYED_GREEN: DeferredHolder<Potion, Potion> =
+		register("dyed_green") { MobEffectInstance(ModMobEffects.DYED_GREEN, POTION_DURATION) }
+	val DYED_BLUE: DeferredHolder<Potion, Potion> =
+		register("dyed_blue") { MobEffectInstance(ModMobEffects.DYED_BLUE, POTION_DURATION) }
+	val DYED_RED: DeferredHolder<Potion, Potion> =
+		register("dyed_red") { MobEffectInstance(ModMobEffects.DYED_RED, POTION_DURATION) }
+	val DYED_YELLOW: DeferredHolder<Potion, Potion> =
+		register("dyed_yellow") { MobEffectInstance(ModMobEffects.DYED_YELLOW, POTION_DURATION) }
+	val DYED_MAGENTA: DeferredHolder<Potion, Potion> =
+		register("dyed_magenta") { MobEffectInstance(ModMobEffects.DYED_MAGENTA, POTION_DURATION) }
+	val DYED_CYAN: DeferredHolder<Potion, Potion> =
+		register("dyed_cyan") { MobEffectInstance(ModMobEffects.DYED_CYAN, POTION_DURATION) }
 
-	private fun registerDyed(wormColor: WormColor): Holder<Potion> {
-		val name = "dyed_${wormColor.colorName}"
-
+	private fun register(
+		name: String,
+		effect: () -> MobEffectInstance
+	): DeferredHolder<Potion, Potion> {
 		return POTION_REGISTRY.register(name, Supplier {
-			Potion(
-				"${CritterCarts.MOD_ID}.$name",
-				MobEffectInstance(ModMobEffects.getDyedEffect(wormColor), POTION_DURATION)
-			)
+			Potion(CritterCarts.MOD_ID + "." + name, effect())
 		})
 	}
 
 	fun registerRecipes(event: RegisterBrewingRecipesEvent) {
-		for (wormColor in WormColor.entries) {
-			event.builder.addMix(
-				Potions.AWKWARD,
-				ModItems.getDyeberry(wormColor).get(),
-				getDyedPotion(wormColor)
-			)
-		}
-	}
+		val builder = event.builder
 
-	fun getDyedPotion(wormColor: WormColor): Holder<Potion> {
-		return when (wormColor) {
-			WormColor.GREEN -> DYED_GREEN
-			WormColor.BLUE -> DYED_BLUE
-			WormColor.RED -> DYED_RED
-			WormColor.YELLOW -> DYED_YELLOW
-			WormColor.MAGENTA -> DYED_MAGENTA
-			WormColor.CYAN -> DYED_CYAN
-		}
+		builder.addMix(Potions.AWKWARD, ModItems.GREEN_DYEBERRY.get(), DYED_GREEN)
+		builder.addMix(Potions.AWKWARD, ModItems.BLUE_DYEBERRY.get(), DYED_BLUE)
+		builder.addMix(Potions.AWKWARD, ModItems.RED_DYEBERRY.get(), DYED_RED)
+		builder.addMix(Potions.AWKWARD, ModItems.YELLOW_DYEBERRY.get(), DYED_YELLOW)
+		builder.addMix(Potions.AWKWARD, ModItems.MAGENTA_DYEBERRY.get(), DYED_MAGENTA)
+		builder.addMix(Potions.AWKWARD, ModItems.CYAN_DYEBERRY.get(), DYED_CYAN)
 	}
 }
