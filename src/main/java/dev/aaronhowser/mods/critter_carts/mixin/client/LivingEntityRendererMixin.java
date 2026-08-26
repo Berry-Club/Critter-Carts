3ee724fe35a762428ea37ebf36f21e064a5af756
aaron <aaronhowser1@gmail.com>
2026-08-26 12:45:34 -0500

    Tint entities with dyed effects

---
+++ src/main/java/dev/aaronhowser/mods/critter_carts/mixin/client/LivingEntityRendererMixin.java
@@ -0,0 +1,47 @@
+package dev.aaronhowser.mods.critter_carts.mixin.client;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.mojang.blaze3d.vertex.PoseStack;
+import dev.aaronhowser.mods.critter_carts.client.render.TintingMultiBufferSource;
+import dev.aaronhowser.mods.critter_carts.entity.data.WormColor;
+import dev.aaronhowser.mods.critter_carts.registry.ModMobEffects;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(LivingEntityRenderer.class)
+public abstract class LivingEntityRendererMixin {
+
+	@WrapMethod(
+			method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V"
+	)
+	private void critterCarts$tintDyedEntity(
+			LivingEntity entity,
+			float entityYaw,
+			float partialTick,
+			PoseStack poseStack,
+			MultiBufferSource bufferSource,
+			int packedLight,
+			Operation<Void> original
+	) {
+		WormColor wormColor = ModMobEffects.INSTANCE.getDyeColor(entity);
+
+		if (wormColor != null) {
+			bufferSource = new TintingMultiBufferSource(
+					bufferSource,
+					wormColor.getTintColor()
+			);
+		}
+
+		original.call(
+				entity,
+				entityYaw,
+				partialTick,
+				poseStack,
+				bufferSource,
+				packedLight
+		);
+	}
+}
