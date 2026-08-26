bd6a69359031db6bb7ba19456695a583ac02b3c5
aaron <aaronhowser1@gmail.com>
2026-08-26 13:39:40 -0500

    property

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/item/DyeberryItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/item/DyeberryItem.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.critter_carts.item
 
 import dev.aaronhowser.mods.critter_carts.entity.data.WormColor
+import dev.aaronhowser.mods.critter_carts.registry.ModDataComponents
 import dev.aaronhowser.mods.critter_carts.registry.ModMobEffects
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.food.FoodProperties
@@ -23,7 +24,9 @@ class DyeberryItem(properties: Properties) : Item(properties) {
 				)
 				.build()
 
-			return Properties().food(foodProperties)
+			return Properties()
+				.food(foodProperties)
+				.component(ModDataComponents.WORM_COLOR, wormColor)
 		}
 	}
 
