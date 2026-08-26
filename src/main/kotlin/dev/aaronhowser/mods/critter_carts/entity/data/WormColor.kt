8533a10b55f636079da3bc1b5c4ae24b641189a7
aaron <aaronhowser1@gmail.com>
2026-08-26 15:09:19 -0500

    WormColor.AARON

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/entity/data/WormColor.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/entity/data/WormColor.kt
@@ -16,7 +16,8 @@ enum class WormColor(
 	RED("red", 0xFFFF5555.toInt()),
 	YELLOW("yellow", 0xFFFFFF55.toInt()),
 	MAGENTA("magenta", 0xFFFF55FF.toInt()),
-	CYAN("cyan", 0xFF55FFFF.toInt());
+	CYAN("cyan", 0xFF55FFFF.toInt()),
+	AARON("aaron", 0xFF5555FF.toInt());
 
 	val headTexture = CritterCarts.modResource("textures/entity/scoochworm/$colorName/head.png")
 	val bodyTexture = CritterCarts.modResource("textures/entity/scoochworm/$colorName/body.png")
