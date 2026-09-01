b922376e440af72733083b3f131285b207e6ed22
aaron <aaronhowser1@gmail.com>
2026-09-01 07:52:30 -0500

    remove on sneak click

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/item/WebFluidItem.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/item/WebFluidItem.kt
@@ -1,10 +1,16 @@
 package dev.aaronhowser.mods.critter_carts.item
 
 import dev.aaronhowser.mods.critter_carts.handler.web.WebLineInteractionHandler
+import dev.aaronhowser.mods.critter_carts.registry.ModDataComponents
 import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
+import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.context.UseOnContext
+import net.minecraft.world.level.Level
 
 class WebFluidItem(properties: Properties) : Item(properties) {
 
@@ -18,7 +24,25 @@ class WebFluidItem(properties: Properties) : Item(properties) {
 			context.clickedFace,
 			context.clickLocation
 		)
+
 		WebLineInteractionHandler.handleNodeSelection(level, player, context.itemInHand, blockAnchor)
 		return InteractionResult.CONSUME
 	}
+
+	override fun use(
+		level: Level,
+		player: Player,
+		usedHand: InteractionHand
+	): InteractionResultHolder<ItemStack> {
+		if (player.isSecondaryUseActive) {
+			val usedStack = player.getItemInHand(usedHand)
+			if (usedStack.has(ModDataComponents.WEB_NODE)) {
+				usedStack.remove(ModDataComponents.WEB_NODE)
+				return InteractionResultHolder.sidedSuccess(usedStack, level.isClientSide)
+			}
+		}
+
+		return super.use(level, player, usedHand)
+	}
+
 }
