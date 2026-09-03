ded114666f652a1eef920f64ab9e8070738e6dc5
aaron <aaronhowser1@gmail.com>
2026-09-03 05:47:15 -0500

    Port configurable item filters

---
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/item/ItemFilterItem.kt
@@ -0,0 +1,93 @@
+package dev.aaronhowser.mods.critter_carts.item
+
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isNotEmpty
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isServerSide
+import dev.aaronhowser.mods.critter_carts.item.component.ItemFilterComponent
+import dev.aaronhowser.mods.critter_carts.menu.item_filter.ItemFilterMenu
+import dev.aaronhowser.mods.critter_carts.registry.ModDataComponents
+import net.minecraft.ChatFormatting
+import net.minecraft.core.NonNullList
+import net.minecraft.network.chat.Component
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.InteractionResultHolder
+import net.minecraft.world.SimpleMenuProvider
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.inventory.MenuConstructor
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.TooltipFlag
+import net.minecraft.world.level.Level
+
+class ItemFilterItem(properties: Properties) : Item(properties) {
+
+	override fun use(
+		level: Level,
+		player: Player,
+		usedHand: InteractionHand
+	): InteractionResultHolder<ItemStack?> {
+		val usedStack = player.getItemInHand(usedHand)
+		if (level.isServerSide) {
+			val constructor = MenuConstructor { containerId, inventory, _ ->
+				ItemFilterMenu(containerId, inventory, usedHand)
+			}
+			player.openMenu(SimpleMenuProvider(constructor, usedStack.hoverName)) { it.writeEnum(usedHand) }
+		}
+		return InteractionResultHolder.sidedSuccess(usedStack, level.isClientSide)
+	}
+
+	override fun appendHoverText(
+		stack: ItemStack,
+		context: TooltipContext,
+		tooltipComponents: MutableList<Component>,
+		tooltipFlag: TooltipFlag
+	) {
+		val component = getFilterComponent(stack)
+		for (flag in component.flags) {
+			tooltipComponents.add(flag.getMessage(true).withStyle(ChatFormatting.BLUE))
+		}
+
+		val filterStacks = component.getItems()
+		for (y in 0 until 4) {
+			val stacks: MutableList<ItemStack> = mutableListOf()
+			for (x in 0 until 4) {
+				val ghostStack = filterStacks.getOrNull(y * 4 + x) ?: continue
+				if (ghostStack.isNotEmpty()) stacks.add(ghostStack)
+			}
+			if (stacks.isEmpty()) continue
+
+			val line = Component.empty()
+			for (index in stacks.indices) {
+				line.append(stacks[index].hoverName)
+				if (index < stacks.lastIndex) line.append(Component.literal(", "))
+			}
+			tooltipComponents.add(line)
+		}
+	}
+
+	companion object {
+		fun getFilterComponent(filterStack: ItemStack): ItemFilterComponent {
+			return filterStack.get(ModDataComponents.ITEM_FILTER) ?: ItemFilterComponent()
+		}
+
+		fun setFlags(filterStack: ItemStack, flags: List<ItemFilterComponent.Flag>) {
+			filterStack.set(ModDataComponents.ITEM_FILTER, getFilterComponent(filterStack).withFlags(flags))
+		}
+
+		fun setStack(filterStack: ItemStack, slot: Int, stackToPlace: ItemStack): Boolean {
+			if (slot !in 0 until ItemFilterComponent.CONTAINER_SIZE) return false
+			filterStack.set(
+				ModDataComponents.ITEM_FILTER,
+				getFilterComponent(filterStack).withSetItem(slot, stackToPlace)
+			)
+			return true
+		}
+
+		fun getFilterItems(filterStack: ItemStack): NonNullList<ItemStack> {
+			return getFilterComponent(filterStack).getItems()
+		}
+
+		fun passesFilter(filterStack: ItemStack, checkedStack: ItemStack): Boolean {
+			return getFilterComponent(filterStack).passesFilter(checkedStack)
+		}
+	}
+}
---
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/menu/item_filter/ItemFilterScreen.kt
@@ -0,0 +1,144 @@
+package dev.aaronhowser.mods.critter_carts.menu.item_filter
+
+import dev.aaronhowser.mods.aaron.menu.BaseScreen
+import dev.aaronhowser.mods.aaron.menu.textures.ScreenBackground
+import dev.aaronhowser.mods.aaron.menu.textures.ScreenSprite
+import dev.aaronhowser.mods.aaron.misc.AaronExtensions.toComponent
+import dev.aaronhowser.mods.aaron.packet.c2s.ClientClickedMenuButton
+import dev.aaronhowser.mods.critter_carts.CritterCarts
+import dev.aaronhowser.mods.critter_carts.datagen.language.ModMenuLang
+import dev.aaronhowser.mods.critter_carts.menu.ToggleSpriteButton
+import net.minecraft.client.gui.components.Button
+import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.player.Inventory
+
+class ItemFilterScreen(
+	menu: ItemFilterMenu,
+	playerInventory: Inventory,
+	title: Component
+) : BaseScreen<ItemFilterMenu>(menu, playerInventory, title) {
+
+	override val background: ScreenBackground = BACKGROUND
+	override val inventoryLabelOffsetY: Int = 73
+
+	private lateinit var invertButton: Button
+	private lateinit var useTagsButton: Button
+	private lateinit var ignoreDamageButton: Button
+	private lateinit var ignoreAllComponentsButton: Button
+
+	override fun baseInit() {
+		super.baseInit()
+
+		val buttonY = topPos + 110
+		val middleX = leftPos + background.width / 2
+
+		val buttonWidth = 20
+		val buttonSpacing = 10
+
+		var buttonX = middleX - (buttonSpacing / 2) - buttonWidth - buttonSpacing - buttonWidth
+
+		invertButton = ToggleSpriteButton(
+			x = buttonX,
+			y = buttonY,
+			width = 20,
+			height = 20,
+			font = font,
+			sprites = Pair(INVERT_ON, INVERT_OFF),
+			messages = Pair(
+				ModMenuLang.INVERTED_ON.toComponent(),
+				ModMenuLang.INVERTED_OFF.toComponent()
+			),
+			isOnGetter = { menu.isInverted() },
+			onPress = {
+				val packet = ClientClickedMenuButton(ItemFilterMenu.TOGGLE_INVERTED_BUTTON_ID)
+				packet.messageServer()
+			}
+		)
+
+		buttonX += buttonWidth + buttonSpacing
+
+		useTagsButton = ToggleSpriteButton(
+			x = buttonX,
+			y = buttonY,
+			width = 20,
+			height = 20,
+			font = font,
+			sprites = Pair(USE_TAGS_ON, USE_TAGS_OFF),
+			messages = Pair(
+				ModMenuLang.USE_TAGS_ON.toComponent(),
+				ModMenuLang.USE_TAGS_OFF.toComponent()
+			),
+			isOnGetter = { menu.useTags() },
+			onPress = {
+				val packet = ClientClickedMenuButton(ItemFilterMenu.TOGGLE_USE_TAGS_BUTTON_ID)
+				packet.messageServer()
+			}
+		)
+
+		buttonX += buttonWidth + buttonSpacing
+
+		ignoreDamageButton = ToggleSpriteButton(
+			x = buttonX,
+			y = buttonY,
+			width = 20,
+			height = 20,
+			font = font,
+			sprites = Pair(IGNORE_DAMAGE_ON, IGNORE_DAMAGE_OFF),
+			messages = Pair(
+				ModMenuLang.IGNORE_DAMAGE_ON.toComponent(),
+				ModMenuLang.IGNORE_DAMAGE_OFF.toComponent()
+			),
+			isOnGetter = { menu.ignoreDamage() },
+			onPress = {
+				val packet = ClientClickedMenuButton(ItemFilterMenu.TOGGLE_IGNORE_DAMAGE_BUTTON_ID)
+				packet.messageServer()
+			}
+		)
+
+		buttonX += buttonWidth + buttonSpacing
+
+		ignoreAllComponentsButton = ToggleSpriteButton(
+			x = buttonX,
+			y = buttonY,
+			width = 20,
+			height = 20,
+			font = font,
+			sprites = Pair(IGNORE_ALL_COMPONENTS_ON, IGNORE_ALL_COMPONENTS_OFF),
+			messages = Pair(
+				ModMenuLang.IGNORE_ALL_COMPONENTS_ON.toComponent(),
+				ModMenuLang.IGNORE_ALL_COMPONENTS_OFF.toComponent()
+			),
+			isOnGetter = { menu.ignoreAllComponents() },
+			onPress = {
+				val packet = ClientClickedMenuButton(ItemFilterMenu.TOGGLE_IGNORE_ALL_COMPONENTS_BUTTON_ID)
+				packet.messageServer()
+			}
+		)
+
+		addRenderableWidget(invertButton)
+		addRenderableWidget(useTagsButton)
+		addRenderableWidget(ignoreDamageButton)
+		addRenderableWidget(ignoreAllComponentsButton)
+	}
+
+	companion object {
+		val BACKGROUND = ScreenBackground(CritterCarts.modResource("textures/gui/item_filter.png"), 176, 241)
+
+		val INVERT_OFF = ScreenSprite(CritterCarts.modResource("filter/invert_off"), 16, 16)
+		val INVERT_ON = ScreenSprite(CritterCarts.modResource("filter/invert_on"), 16, 16)
+		val USE_TAGS_OFF = ScreenSprite(CritterCarts.modResource("filter/use_tags_off"), 16, 16)
+		val USE_TAGS_ON = ScreenSprite(CritterCarts.modResource("filter/use_tags_on"), 16, 16)
+		val IGNORE_DAMAGE_OFF = ScreenSprite(CritterCarts.modResource("filter/ignore_damage_off"), 16, 16)
+		val IGNORE_DAMAGE_ON = ScreenSprite(CritterCarts.modResource("filter/ignore_damage_on"), 16, 16)
+		val IGNORE_ALL_COMPONENTS_OFF = ScreenSprite(
+			CritterCarts.modResource("filter/ignore_all_components_off"),
+			16,
+			16
+		)
+		val IGNORE_ALL_COMPONENTS_ON = ScreenSprite(
+			CritterCarts.modResource("filter/ignore_all_components_on"),
+			16,
+			16
+		)
+	}
+}
