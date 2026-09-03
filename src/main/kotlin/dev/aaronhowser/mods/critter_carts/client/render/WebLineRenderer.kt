6a6e202d84d64bdd0f99c57d51bb8c95a2e65c9d
aaron <aaronhowser1@gmail.com>
2026-09-03 04:01:09 -0500

    render lines as models with a texture

--- src/main/kotlin/dev/aaronhowser/mods/critter_carts/client/render/WebLineRenderer.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critter_carts/client/render/WebLineRenderer.kt
@@ -1,6 +1,7 @@
 package dev.aaronhowser.mods.critter_carts.client.render
 
 import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.blaze3d.vertex.VertexConsumer
 import dev.aaronhowser.mods.aaron.client.render.AaronRenderUtil
 import dev.aaronhowser.mods.aaron.misc.AaronDsls.withPose
 import dev.aaronhowser.mods.aaron.misc.AaronExtensions.isItem
@@ -12,6 +13,10 @@ import dev.aaronhowser.mods.critter_carts.handler.web.node.WebNode
 import dev.aaronhowser.mods.critter_carts.registry.ModDataComponents
 import dev.aaronhowser.mods.critter_carts.registry.ModItems
 import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.LightTexture
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.client.renderer.texture.OverlayTexture
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.world.phys.HitResult
@@ -20,6 +25,8 @@ import net.neoforged.api.distmarker.Dist
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent
+import org.joml.Matrix4f
+import org.joml.Quaternionf
 
 @EventBusSubscriber(
 	modid = CritterCarts.MOD_ID,
@@ -39,7 +46,7 @@ object WebLineRenderer {
 			poseStack.translate(-cameraPosition.x, -cameraPosition.y, -cameraPosition.z)
 
 			for (line in ClientWebLines.getLines()) {
-				AaronRenderUtil.renderLineThroughWalls(
+				renderWebLine(
 					poseStack,
 					line.firstNode.position,
 					line.secondNode.position,
@@ -72,12 +79,108 @@ object WebLineRenderer {
 		)
 		val color = if (isValid) VALID_PREVIEW_COLOR else INVALID_PREVIEW_COLOR
 
-		AaronRenderUtil.renderLineThroughWalls(
+		renderWebLine(
 			poseStack,
 			firstNode.position,
 			secondNode.position,
 			color
 		)
+	}
+
+	private fun renderWebLine(
+		poseStack: PoseStack,
+		start: Vec3,
+		end: Vec3,
+		color: Int
+	) {
+		val offset = end.subtract(start)
+		val height = offset.length()
+		if (height == 0.0) return
+
+		val minecraft = Minecraft.getInstance()
+		val bufferSource = minecraft.renderBuffers().bufferSource()
+		val vertexConsumer = bufferSource.getBuffer(WEB_RENDER_TYPE)
+		val direction = offset.scale(1.0 / height)
+		val rotation = Quaternionf().rotationTo(
+			0f,
+			1f,
+			0f,
+			direction.x.toFloat(),
+			direction.y.toFloat(),
+			direction.z.toFloat()
+		)
+
+		poseStack.withPose {
+			poseStack.translate(start.x, start.y, start.z)
+			poseStack.mulPose(rotation)
+
+			val pose = poseStack.last().pose()
+
+			addSide(
+				vertexConsumer, pose,
+				-WEB_RADIUS, -WEB_RADIUS,
+				WEB_RADIUS, -WEB_RADIUS,
+				height, 0f, 0.25f, color
+			)
+			addSide(
+				vertexConsumer, pose,
+				WEB_RADIUS, -WEB_RADIUS,
+				WEB_RADIUS, WEB_RADIUS,
+				height, 0.25f, 0.5f, color
+			)
+			addSide(
+				vertexConsumer, pose,
+				WEB_RADIUS, WEB_RADIUS,
+				-WEB_RADIUS, WEB_RADIUS,
+				height, 0.5f, 0.75f, color
+			)
+			addSide(
+				vertexConsumer, pose,
+				-WEB_RADIUS, WEB_RADIUS,
+				-WEB_RADIUS, -WEB_RADIUS,
+				height, 0.75f, 1f, color
+			)
+		}
+
+		bufferSource.endBatch(WEB_RENDER_TYPE)
+	}
+
+	private fun addSide(
+		vertexConsumer: VertexConsumer,
+		pose: Matrix4f,
+		firstX: Float,
+		firstZ: Float,
+		secondX: Float,
+		secondZ: Float,
+		height: Double,
+		minU: Float,
+		maxU: Float,
+		color: Int
+	) {
+		val maxV = (height / TEXTURE_REPEAT_DISTANCE).toFloat()
+
+		addVertex(vertexConsumer, pose, firstX, 0f, firstZ, minU, 0f, color)
+		addVertex(vertexConsumer, pose, secondX, 0f, secondZ, maxU, 0f, color)
+		addVertex(vertexConsumer, pose, secondX, height.toFloat(), secondZ, maxU, maxV, color)
+		addVertex(vertexConsumer, pose, firstX, height.toFloat(), firstZ, minU, maxV, color)
+	}
+
+	private fun addVertex(
+		vertexConsumer: VertexConsumer,
+		pose: Matrix4f,
+		x: Float,
+		y: Float,
+		z: Float,
+		u: Float,
+		v: Float,
+		color: Int
+	) {
+		vertexConsumer.addVertex(pose, x, y, z)
+			.setColor(color)
+			.setUv(u, v)
+			.setOverlay(OverlayTexture.NO_OVERLAY)
+			.setLight(LightTexture.FULL_BRIGHT)
+			.setNormal(0f, 1f, 0f)
 	}
 
 	private fun getHeldWebFluid(mainHandItem: ItemStack, offhandItem: ItemStack): ItemStack? {
@@ -139,4 +242,12 @@ object WebLineRenderer {
 	private const val WEB_COLOR = 0xFFFFFFFF.toInt()
 	private const val VALID_PREVIEW_COLOR = 0xFFFFFFFF.toInt()
 	private const val INVALID_PREVIEW_COLOR = 0xFFFF0000.toInt()
+	private const val WEB_RADIUS = 1f / 64f
+	private const val TEXTURE_REPEAT_DISTANCE = 8.0
+
+	private val WEB_TEXTURE = ResourceLocation.fromNamespaceAndPath(
+		CritterCarts.MOD_ID,
+		"textures/misc/web_line.png"
+	)
+	private val WEB_RENDER_TYPE = RenderType.entityCutoutNoCull(WEB_TEXTURE)
 }
